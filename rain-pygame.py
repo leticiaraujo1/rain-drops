3070f78ca82efe59ab37d72b2f6ca16d2c3dc051
Leticia Araujo <analeticia.araujo05@gmail.com>
2024-03-17 00:22:33 -0300

    updated class and code separated into functions

--- rain-pygame.py
+++ rain-pygame.py
@@ -1,56 +1,61 @@
 import pygame 
 import random
 
-pygame.init() 
-
-width = 500
-height = 350
-screen = pygame.display.set_mode((width,height)) 
-
-blue = (0,0,255)
-
-y = 0
-
-clock = pygame.time.Clock()
-
-rain_sound = 'rain.wav'
-
-class RainDrops:
-	def __init__(self, surface, color, x, y):
+class RainDrops():
+	def __init__(self, surface, color, start_x, start_y):
 		self.surface = surface
 		self.color = color
-		self.x = x
-		self.y = y
+		self.x = start_x
+		self.y = start_y
+		self.sy = start_y
 		self.sizeX = 2
 		self.sizeY = 10
 		self.type = 'rect'
-		self.create_Object()
-	
-	def create_Object(self):
-		if self.type == 'rect':
-			self.draw() 
 
 	def draw(self):
 		return pygame.draw.rect(self.surface, self.color, (self.x, self.y, self.sizeX, self.sizeY))
 
-	def fall():
-		return True
+	def fall(self):
+		if self.y == 350:
+			self.y = self.sy
+		else:
+			self.y += 1
 
-for i in range(60):
-	RainDrops(screen, blue, random.randint(0, width), y)
-	
+def main():
+	pygame.init()
 
-pygame.display.flip()
+	width = 500
+	height = 350
+	screen = pygame.display.set_mode((width,height))
+
+	blue = (0,0,255) 
+	y = 0
+	clock = pygame.time.Clock()
+	rain_sound = 'rain.wav'
+
+	rain_drops = []
+	for i in range(60):
+		rain_drops.append(RainDrops(screen, blue, random.randint(0, width), y))
+
+	pygame.mixer.music.load(rain_sound)
+	pygame.mixer.music.play(-1)
+
+	run = True
+	while run: 
+		
+		for event in pygame.event.get(): 
+			if event.type == pygame.QUIT: 
+				run = False
+		
+		for rect in rain_drops:
+			rect.fall()
+		
+		pygame.display.flip()
+		clock.tick(60)
+
+if __name__ == "__main__":
+    main()
 
 
-pygame.mixer.music.load(rain_sound)
-pygame.mixer.music.play(-1)
 
-run = True
- 
-while run: 
-	
-	for event in pygame.event.get(): 
-		if event.type == pygame.QUIT: 
-			run = False
 
