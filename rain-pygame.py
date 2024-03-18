404a0a6ce9c2f962a37cbd5ff108d8559cacec74
Leticia Araujo <analeticia.araujo05@gmail.com>
2024-03-18 00:08:09 -0300

    updated code using pygame.sprite

--- rain-pygame.py
+++ rain-pygame.py
@@ -1,25 +1,20 @@
 import pygame 
 import random
 
-class RainDrops():
-	def __init__(self, surface, color, start_x, start_y):
-		self.surface = surface
-		self.color = color
-		self.x = start_x
-		self.y = start_y
-		self.sy = start_y
-		self.sizeX = 2
-		self.sizeY = 10
-		self.type = 'rect'
+class RainDrops(pygame.sprite.Sprite):
+	def __init__(self, pos_x, pos_y, size_x, size_y, speed):
+		super().__init__()
+		self.image = pygame.Surface((size_x, size_y))
+		self.image.fill((54, 125, 201)) 
+		self.rect = self.image.get_rect()
+		self.rect.x = pos_x
+		self.rect.y = pos_y
+		self.speed = speed
 
-	def draw(self):
-		return pygame.draw.rect(self.surface, self.color, (self.x, self.y, self.sizeX, self.sizeY))
-
-	def fall(self):
-		if self.y == 350:
-			self.y = self.sy
-		else:
-			self.y += 1
+	def update(self):
+		self.rect.y += self.speed
+		if self.rect.y >= 350:
+			self.rect.y = 0
 
 def main():
 	pygame.init()
@@ -28,14 +23,13 @@ def main():
 	height = 350
 	screen = pygame.display.set_mode((width,height))
 
-	blue = (0,0,255) 
-	y = 0
 	clock = pygame.time.Clock()
 	rain_sound = 'rain.wav'
 
-	rain_drops = []
-	for i in range(60):
-		rain_drops.append(RainDrops(screen, blue, random.randint(0, width), y))
+	rain_drops = pygame.sprite.Group()
+
+	for i in range(500):
+		rain_drops.add(RainDrops(random.randint(0, width), random.randint(0, height), 2, 8, 5))
 
 	pygame.mixer.music.load(rain_sound)
 	pygame.mixer.music.play(-1)
@@ -47,15 +41,16 @@ def main():
 			if event.type == pygame.QUIT: 
 				run = False
 		
-		for rect in rain_drops:
-			rect.fall()
+		rain_drops.update()
+
+		screen.fill((0, 0, 0))
+		rain_drops.draw(screen)
 		
 		pygame.display.flip()
 		clock.tick(60)
 
 if __name__ == "__main__":
     main()
-
 
 
 
